fde828f1648b7ff404f046a3d4f04fdf7b791c5b
Arma <armaqanalibabaei@gmail.com>
2024-05-30 10:58:04 +0330

    "Added about/Contact/home"

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+from django.template import loader
+
+
+def home(request):
+    template = loader.get_template('home.html').render()
+    return HttpResponse(template)
+
+
+def about(request):
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render())
+
+
+def contact(request):
+    template = loader.get_template('contact.html').render()
+    return HttpResponse(template)
